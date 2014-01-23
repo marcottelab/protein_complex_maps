a5256de01e82ef93c25d45e8dd065a6364fc8204
Kevin Drew <kdrew@utexas.edu>
2014-01-23 16:37:00 -0600

    compute_benchmark_probs is a driver script to iterate through the pdb benchmark, call relative stoichiometry function and store results.
    analyze_benchmark is an analysis script which compares predictions to true stoichiometries in benchmark
    
    normalize_benchmark_msds is a pre-conditioning script to normalize the data based on length and threshold (experimental).
    
    With this iteration of the code, out of 113 pdbs in benchmark (has greater than 10 data points/ calculated ratios:
    score: 3740
    top_ten: 15
    top_one: 8
    
    score is the sum of all the top ranks in the benchmark. Low is better where rank 0 is top hit was correct.  Average rank is 33. Not great but just a first pass.

---
+++ protein_complex_maps/stoichiometry/benchmark/analyze_benchmark.py
@@ -0,0 +1,62 @@
+
+
+import itertools as it
+import cPickle 
+import operator
+import ast
+
+import protein_complex_maps.protein_util as pu
+
+
+#kdrew: pickle comes from running protein_complex_maps.stoichiometry.benchmark.build_pdb_benchmark.py
+mscpdbs = cPickle.load(open("/home/kdrew/scripts/protein_complex_maps/protein_complex_maps/stoichiometry/benchmark/ms_complete_pdbs.p", "rb"))
+
+#kdrew: pickle comes from protein_complex_maps.stoichiometry.benchmark.compute_benchmark_probs.py
+mscpdbs_results = cPickle.load(open("/home/kdrew/scripts/protein_complex_maps/protein_complex_maps/stoichiometry/benchmark/HS_ms2_elutions_msds_ms_complete_pdbs_results.p", "rb"))
+
+#print mscpdbs_results
+#print mscpdbs
+
+score = 0
+top_ten = 0
+top_one = 0
+for pdb_id in mscpdbs_results:
+	sorted_results = sorted(mscpdbs_results[pdb_id][0].iteritems(), key=operator.itemgetter(1))
+	sorted_results.reverse()
+	if mscpdbs_results[pdb_id][1] > 10:
+		print pdb_id
+		print "true: %s" % (mscpdbs[pdb_id],)
+		print "lengths: %s" % (pu.get_length_uniprot( mscpdbs[pdb_id].keys() ))
+		print "data_points: %s" % (mscpdbs_results[pdb_id][1],)
+		print "mapping: %s" % (mscpdbs_results[pdb_id][2],)
+		true_map = {}
+		for chain in mscpdbs_results[pdb_id][2]:
+			true_map[chain] = mscpdbs[pdb_id][mscpdbs_results[pdb_id][2][chain]]
+		print "true_map: %s" % (true_map,)
+		top_rank = None
+		for rank, pred in enumerate(sorted_results):
+			stoich = ast.literal_eval(pred[0])
+			print "rank: %s, stoich: %s, score: %s" % (rank, stoich, pred[1])
+			matched_all = True
+			for comb in it.combinations(stoich.keys(),2):
+				true_ratio = 1.0*true_map[comb[0]]/true_map[comb[1]]
+				pred_ratio = 1.0*stoich[comb[0]]/stoich[comb[1]]
+				if true_ratio != pred_ratio:
+					matched_all = False
+
+			#print "match?: %s" % (matched_all,)
+			if matched_all and top_rank == None:
+				top_rank  = rank
+
+		print "top_rank: %s" % (top_rank,)
+		score += top_rank
+		if top_rank < 10:
+			top_ten += 1
+		if top_rank == 0:
+			top_one += 1
+		
+print "score: %s" % (score,)
+print "top_ten: %s" % (top_ten,)
+print "top_one: %s" % (top_one,)
+
+
---
+++ protein_complex_maps/stoichiometry/benchmark/compute_benchmark_probs.py
@@ -0,0 +1,50 @@
+
+
+import glob
+import cPickle
+import pickle
+
+import protein_complex_maps.read_data as rd
+import protein_complex_maps.normalization_util as nu
+import protein_complex_maps.stoichiometry.stoichiometry as st
+import protein_complex_maps.stoichiometry.relative_stoichiometry as rs
+
+
+def main():
+
+	#kdrew: read in benchmark
+	#kdrew: pickle comes from running protein_complex_maps.stoichiometry.benchmark.build_pdb_benchmark.py
+	ms_complete_pdbs = cPickle.load(open("./ms_complete_pdbs.p", "rb"))
+
+	msds = None
+	#kdrew: pickle comes from running protein_complex_maps.stoichiometry.benchmark.normalize_benchmark_msds.py with length_normalize = True
+	msds = pickle.load( open( "./HS_ms2_elutions_msds_lenNormal.p", "rb" ) )
+	print msds.get_id_dict()
+
+
+	#kdrew: read in stoichiometries, file is from pdb website (slightly massaged)
+	filename = "/home/kdrew/scripts/protein_complex_maps/protein_complex_maps/stoichiometry/benchmark/pdb_stoichiometry_list.txt"
+	sample_file = open(filename, 'rb')
+	s_set = st.Stoichiometries()
+	s_set.read_stoichiometries(sample_file)
+
+	msds_id_dict = msds.get_id_dict()
+	msds_id_set = set(msds_id_dict.keys())
+	results_dict = dict()
+	#kdrew: for every benchmark complex, call relative stoichiometry
+	for pdb_id in ms_complete_pdbs:
+		print pdb_id
+		prot_ids = ms_complete_pdbs[pdb_id].keys()
+		if msds_id_set >= set(prot_ids):
+			print "benchmark ids are in msds"
+			results, num_dpts, prot_ids_map = rs.relative_stoichiometry( msds, prot_ids, s_set )
+			print results
+			results_dict[pdb_id] = (results, num_dpts, prot_ids_map)
+		else:
+			print "benchmark ids are NOT in msds"
+
+	cPickle.dump(results_dict, open("./HS_ms2_elutions_msds_lenNormal_ms_complete_pdbs_results.p", "wb"))
+
+if __name__ == "__main__":
+	main()
+
