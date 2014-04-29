90b4a365a4471284ead50b476f15963676df1bcd
Kevin Drew <kdrew@utexas.edu>
2014-04-29 17:01:09 -0500

    Added functionality to reorder data matrix so bicluster rows and columns are contiguous.
    Also, added flag to only plot biclusters with scores meeting a given
    threshold

--- protein_complex_maps/coclustering.py
+++ protein_complex_maps/coclustering.py
@@ -29,7 +29,7 @@ def main():
 	parser = argparse.ArgumentParser(description="Coclusters fractionation data")
 	parser.add_argument("--input_msds_pickle", action="store", dest="msds_filename", required=True, 
 						help="Filename of MSDS pickle: pickle comes from running protein_complex_maps.util.read_ms_elutions_pickle_MSDS.py")
-	parser.add_argument("--numOfClusters", action="store", dest="numOfClusters", required=True,
+	parser.add_argument("--numOfClusters", action="store", type=int, dest="numOfClusters", required=True,
 						help="Expected number of clusters, (int)")
 	parser.add_argument("--proteins", action="store", dest="proteins", nargs='+', required=False, 
 						help="Protein ids in which to anaylze")
@@ -39,6 +39,8 @@ def main():
 						help="Filename of complex protein identifiers")
 	parser.add_argument("--plot_filename", action="store", dest="plot_filename", required=False, default=None,
 						help="Filename of output plot")
+	parser.add_argument("--plot_threshold", action="store", type=float, dest="plot_threshold", required=False, default=0.0,
+						help="Plot bicluster if score (energy) is less than threshold")
 	#parser.add_argument("-j", action="store", dest="numOfProcs", required=False, default=1,
 	#    				help="Number of processors to use, default=1")
 
@@ -68,7 +70,7 @@ def main():
 
 
 	fit_model = runCluster(data_set, args.numOfClusters)
-	biclusters = evaluateClusters(msds, data_set, new_id_map, fit_model, args.plot_filename)
+	biclusters = evaluateClusters(msds, data_set, new_id_map, fit_model, args.plot_filename, plot_threshold=args.plot_threshold)
 
 def runCluster(data_set, numOfClusters): 
 
@@ -88,7 +90,7 @@ def runCluster(data_set, numOfClusters):
 	return model
 
 
-def evaluateClusters(msds, data_set, new_id_map, fit_model, plot_filename=None):
+def evaluateClusters(msds, data_set, new_id_map, fit_model, plot_filename=None, plot_reordered=True, plot_threshold=0.0):
 
 	bicluster_list = []
 
@@ -111,12 +113,17 @@ def evaluateClusters(msds, data_set, new_id_map, fit_model, plot_filename=None):
 		#kdrew: matplotlib gets whiny about subplots when only one protein in plot, also make sure there are columns in matrix
 		if len(bicluster_ids) > 1 and np.any(fit_model.columns_[j]):
 			bicluster_data_set, bicluster_id_map = msds.get_subdata_matrix(bicluster_ids) 
+
 			bicluster_data_set = bicluster_data_set[np.ix_(range(len(bicluster_data_set)), fit_model.columns_[j])]
+
+
 
 			score = rsscore_obj.zscore_all_neg(bicluster_data_set)
 			print "initial score: %s" % (score,)
 
-			bicluster_list.append((bicluster_ids, score))
+			row_list = list(np.ix_(fit_model.rows_[j])[0]) 
+			column_list = list(np.ix_(fit_model.columns_[j])[0])
+			bicluster_list.append((bicluster_ids, score, row_list, column_list))
 
 
 			#MC_OPT = False
@@ -145,10 +152,17 @@ def evaluateClusters(msds, data_set, new_id_map, fit_model, plot_filename=None):
 
 			#print bc_msds.get_id_dict()
 
-			if plot_filename != None:
+			if plot_filename != None and score < plot_threshold:
 				plot_fname = os.path.splitext(plot_filename)[0]+'.'+str(j)+os.path.splitext(plot_filename)[1]
 				#print bicluster_ids
-				pp.plot_profile( bc_msds, bicluster_ids, savefilename=plot_fname )
+				if plot_reordered:
+					reordered_data_set, reordered_map = msds.reordered_data_matrix(row_list, column_list, data_set, new_id_map)
+					numOfRows = sum(fit_model.rows_[j])
+					numOfColumns = sum(fit_model.columns_[j])
+					#kdrew: since we reordered, highlight the first # of rows and columns
+					pp.plot_profile_dataset( reordered_data_set, reordered_map, savefilename=plot_fname, x_highlight= (0,numOfRows), y_highlight= (0,numOfColumns))
+				else:
+					pp.plot_profile( bc_msds, bicluster_ids, savefilename=plot_fname )
 
 
 		print "\n"
