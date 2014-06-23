4f65e4ac6dcf3a6986dd2cb5659763f1f51b08fa
Kevin Drew <kdrew@utexas.edu>
2014-06-23 11:35:52 -0500

    Updated ortholog_formatter so that it will query for uniprot ids rather than just use the inputted ids.  This is more standard with inparanoid's downloadable files: http://inparanoid.sbc.su.se/download/8.0_current/Orthologs/

--- protein_complex_maps/util/ortholog_formatter.py
+++ protein_complex_maps/util/ortholog_formatter.py
@@ -4,6 +4,15 @@ import glob
 import cPickle
 import argparse
 
+import protein_complex_maps.protein_util as pu
+
+class OrthologEntry(object):
+	def __init__(self, cluster_id, guid, species, score, protid):
+		self.cluster_id = cluster_id
+		self.guid = guid
+		self.species = species
+		self.score = score
+		self.protid = protid
 
 def main():
 
@@ -16,6 +25,12 @@ def main():
 						help="species listed in OrtoA column")
 	parser.add_argument("--species2", action="store", dest="species2", required=True, 
 						help="species listed in OrtoB column")
+	parser.add_argument("--species1_from_id", action="store", dest="species1_from_id", required=True, 
+						help="species1 id type")
+	parser.add_argument("--species2_from_id", action="store", dest="species2_from_id", required=True, 
+						help="species2 id type")
+	parser.add_argument("--to_id", action="store", dest="to_id", required=False, default="ACC", 
+				help="convert ids this type, use empty string for original ids (note: automatically converts gene_exact to ACC)")
 
 	args = parser.parse_args()
 
@@ -29,6 +44,10 @@ def main():
 		return
 
 	outfile = open(args.out_filename, "wb")
+
+	ortholog_entry_list = []
+	prot_s1_list = []
+	prot_s2_list = []
 		
 	file1 = open(args.filename, 'rb')
 	for line in file1.readlines():
@@ -39,11 +58,38 @@ def main():
 		species2_list = col_split[3].split()
 
 		for prot, score in zip(species1_list[0::2], species1_list[1::2]):
-			outfile.write("%s\t%s\t%s\t%s\t%s\n" % (cluster_id, guid, args.species1, score, prot,))
+			#outfile.write("%s\t%s\t%s\t%s\t%s\n" % (cluster_id, guid, args.species1, score, prot,))
+			ortholog_entry_list.append(OrthologEntry(cluster_id, guid, args.species1, score, prot))
+			prot_s1_list.append(prot)
 
 		for prot, score in zip(species2_list[0::2], species2_list[1::2]):
-			outfile.write("%s\t%s\t%s\t%s\t%s\n" % (cluster_id, guid, args.species2, score, prot,))
+			#outfile.write("%s\t%s\t%s\t%s\t%s\n" % (cluster_id, guid, args.species2, score, prot,))
+			ortholog_entry_list.append(OrthologEntry(cluster_id, guid, args.species2, score, prot))
+			prot_s2_list.append(prot)
 
+
+	if args.to_id != "":
+		if args.species1_from_id == "gene_exact":
+			s1_map = pu.get_from_uniprot_by_genename(prot_s1_list)
+		else:
+			s1_map = pu.map_protein_ids(prot_s1_list, from_id=args.species1_from_id, to_id=args.to_id)
+
+		if args.species2_from_id == "gene_exact":
+			s2_map = pu.get_from_uniprot_by_genename(prot_s2_list)
+		else:
+			s2_map = pu.map_protein_ids(prot_s2_list, from_id=args.species1_from_id, to_id=args.to_id)
+
+		ortholog_map = dict(s1_map.items() + s2_map.items())
+
+	for entry in ortholog_entry_list:
+		if args.to_id == "":
+			outfile.write("%s\t%s\t%s\t%s\t%s\n" % (entry.cluster_id, entry.guid, entry.species, entry.score, entry.protid,))
+		else:
+			try: 
+				for o_id in ortholog_map[entry.protid]:
+					outfile.write("%s\t%s\t%s\t%s\t%s\n" % (entry.cluster_id, entry.guid, entry.species, entry.score, o_id,))
+			except KeyError:
+				outfile.write("%s\t%s\t%s\t%s\t%s\n" % (entry.cluster_id, entry.guid, entry.species, entry.score, entry.protid,))
 
 	file1.close()
 	outfile.close()
