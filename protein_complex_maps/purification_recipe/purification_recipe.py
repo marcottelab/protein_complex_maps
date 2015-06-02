c9c2a7b9231ca6720aa6d9106bf27f568bbb98fe
Kevin Drew <kdrew@utexas.edu>
2015-06-02 11:34:25 -0500

    Added threshold for percent of proteins retained in fractions.  Created result object to store fractions and their scores.
    Still need to generalize the process so I it is easier to do more than
    two fractions

--- protein_complex_maps/purification_recipe/purification_recipe.py
+++ protein_complex_maps/purification_recipe/purification_recipe.py
@@ -43,8 +43,12 @@ def main():
                                     help="Filenames of MSDS pickle: pickle comes from running protein_complex_maps.util.read_ms_elutions_pickle_MSDS.py")
 	parser.add_argument("--proteins", action="store", dest="proteins", nargs='+', required=True, 
                                     help="Protein ids in which to anaylze")
+	parser.add_argument("--protein_percent_threshold", action="store", dest="protein_percent_threshold", type=float, required=False, default=0.9,
+                                    help="Percentage of proteins that need to be present in input fractions (expressed as decimal), default 0.9 (ie 90%)")
 
 	args = parser.parse_args()
+
+        results_list = []
 
         df_dict = dict()
         #kdrew: store data in data frames
@@ -67,6 +71,15 @@ def main():
             #kdrew: normalize by fractions (sum of all fraction vectors = 1.0, i.e. probability)
             df = df.div( df.sum(axis=1), axis=0 )
 
+            #kdrew: count how many input proteins are present (> 0.0)
+            #protein_count = (combined_df > 0.0).sum()
+            #print "protein_count %s / %s = %s" % (protein_count, len(args.proteins), (1.0*protein_count)/len(args.proteins))
+
+            #kdrew: threshold out fraction vectors that do not have enough of the input proteins
+            percent_vector = 1.0*(df[args.proteins] > 0.0).sum(axis=1)/len(args.proteins)
+            df = df[percent_vector > args.protein_percent_threshold]
+            print df
+
             df_dict[ msds_filename ] = df
 
             #for prot in args.proteins:
@@ -74,6 +87,7 @@ def main():
 
 
 
+        #kdrew: testing
         #kdrew: score each by summing and print out top fraction in each fractionation experiment
         for exp in df_dict:
             df = df_dict[exp]
@@ -82,11 +96,15 @@ def main():
 
             df_scored = df * score_mask
 
-            top_score_fraction = df_scored.sum(axis=1).idxmax()
+            try:
+                top_score_fraction = df_scored.sum(axis=1).idxmax()
 
-            #print df_scored
-            print "%s %s" % (top_score_fraction, df_scored.loc[top_score_fraction].sum())
+                #print df_scored
+                print "%s %s" % (top_score_fraction, df_scored.loc[top_score_fraction].sum())
+            except ValueError:
+                print "no fraction passed threshold"
 
+        #kdrew: testing single experiment
         for x in list(df_dict[df_dict.keys()[0]].index):
             df1 = df_dict[df_dict.keys()[0]]
             print "df1: %s" % df1
@@ -112,6 +130,7 @@ def main():
             #print "df_scored: %s" % df_scored
             #print "percent score %s: %s" % (x, (df_scored.sum()))
 
+        #kdrew: testing another single experiment
         for x in it.product(list(df_dict[df_dict.keys()[1]].index)):
             df1 = df_dict[df_dict.keys()[1]]
             print "df1: %s" % df1
@@ -138,27 +157,56 @@ def main():
             #print "percent score %s: %s" % (x, (df_scored.sum()))
 
 
-        for x in it.product(list(df_dict[df_dict.keys()[0]].index), list(df_dict[df_dict.keys()[1]].index)):
-            df1 = df_dict[df_dict.keys()[0]]
-            df2 = df_dict[df_dict.keys()[1]]
-            print x
-            print df1.loc[x[0]]
-            print df2.loc[x[1]]
+        for exp1,exp2 in it.combinations( df_dict.keys(), 2 ):
+            print "combinations: %s, %s" % (exp1, exp2)
 
-            combined_df = df1.loc[x[0]].mul( df2.loc[x[1]], fill_value=0.0 )
+            for x in it.product(list(df_dict[exp1].index), list(df_dict[exp2].index)):
 
-            #kdrew: this scoring function gives a 1*abundance to proteins within in the complex and a -1*abundance to all other proteins
-            #kdrew: the sum is the total score 
-            score_mask_1neg1 = [ 1 if i in args.proteins else -1 for i in combined_df.index]
-            df_scored = combined_df * score_mask_1neg1
-            print "score %s: %s" % (x, df_scored.sum())
+                df1 = df_dict[exp1]
+                df2 = df_dict[exp2]
+                print x
+                print df1.loc[x[0]]
+                print df2.loc[x[1]]
+
+                combined_df = df1.loc[x[0]].mul( df2.loc[x[1]], fill_value=0.0 )
+
+                #kdrew: this scoring function gives a 1*abundance to proteins within in the complex and a -1*abundance to all other proteins
+                #kdrew: the sum is the total score 
+                score_mask_1neg1 = [ 1 if i in args.proteins else -1 for i in combined_df.index]
+                df_scored = combined_df * score_mask_1neg1
+                print "score %s: %s" % (x, df_scored.sum())
+
+                #kdrew: count how many original proteins are still present (> 0.0)
+                protein_count = (combined_df[args.proteins] > 0.0).sum()
+                proteins = combined_df.index[(combined_df[args.proteins] > 0.0)].tolist()
+                protein_percent = (1.0*protein_count)/len(args.proteins)
+                print "protein_count %s / %s = %s" % (protein_count, len(args.proteins), protein_percent)
+
+                #kdrew: this scoring function gives what percent the final solution will have of proteins in the complex
+                score_mask_1_0 = [ 1 if i in args.proteins else 0 for i in combined_df.index]
+                df_scored = combined_df * score_mask_1_0
+                purity_percent = df_scored.sum()/combined_df.sum()
+                print "percent score %s: %s" % (x, purity_percent)
+
+                pr = PurificationRecipe(experiments=[exp1,exp2], fractions=x, proteins=proteins, protein_percent=protein_percent, purity_percent=purity_percent)
+
+                results_list.append(pr)
 
 
-            #kdrew: this scoring function gives what percent the final solution will have of proteins in the complex
-            score_mask_1_0 = [ 1 if i in args.proteins else 0 for i in combined_df.index]
-            df_scored = combined_df * score_mask_1_0
-            print "percent score %s: %s" % (x, df_scored.sum()/combined_df.sum())
+        for result in results_list:
+            print result
 
+class PurificationRecipe(object):
+
+    def __init__(self, experiments=[], fractions=[], proteins=[], protein_percent=None, purity_percent=None):
+        self.experiments = experiments
+        self.fractions = fractions
+        self.proteins = proteins
+        self.protein_percent = protein_percent
+        self.purity_percent = purity_percent
+
+    def __str__(self,):
+        return "fractions: %s, protein_percent: %s, purity_percent: %s" % (self.fractions, self.protein_percent, self.purity_percent)
 
 if __name__ == "__main__":
 	main()
