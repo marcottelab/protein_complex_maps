3848574b6672e4a703611733b24c18b7c0d39402
Kevin Drew <kdrew@utexas.edu>
2015-08-05 22:23:55 -0500

    Added flags to parse uniprot ids from file, get fraction name from filename, remove zero entries

--- protein_complex_maps/util/msblender2elution.py
+++ protein_complex_maps/util/msblender2elution.py
@@ -13,21 +13,24 @@ def main():
 						help="Filenames of MSblender prot_count files")
 	parser.add_argument("--output_filename", action="store", dest="output_filename", required=True,
 						help="Filenames of MSblender prot_count files")
-	parser.add_argument("--add_total_count", action="store_true", dest="add_total_count", required=False, default=False,
+	parser.add_argument("--msblender_format", action="store_true", dest="msblender_format", required=False, default=False,
 						help="Original file format has the first column as the sum of all the columns, default=False")
 	parser.add_argument("--spectral_count_type", action="store", dest="spectral_count_type", required=False, default='Unique',
 						help="Which spectral count type should be stored? Unweighted, Weighted or Unique (default)")
 	parser.add_argument("--group", action="store_true", dest="group", required=False, default=False,
 						help="Flag for reading in msblender .group files, default=False")
+	parser.add_argument("--remove_zero_unique", action="store_true", dest="remove_zero_unique", required=False, default=False,
+						help="Flag for removing entries with zero unique matches, default=False")
+	parser.add_argument("--fraction_name_from_filename", action="store_true", dest="fraction_name_from_filename", required=False, default=False,
+						help="Flag for parsing fraction name from filename, default=False, defaults to sequential numbering")
+	parser.add_argument("--parse_uniprot_id", action="store_true", dest="parse_uniprot_id", required=False, default=False,
+						help="Parse out the uniprot id from the index and store in index, default=False")
 
 	args = parser.parse_args()
 
 	elution_profile_df = pd.DataFrame() 
 	all_group_id_list = []
-	for filename in args.prot_count_files:
-		fraction_name = os.path.basename(filename).split('.')[0]
-		print fraction_name
-
+	for i, filename in enumerate(args.prot_count_files):
 		if args.group:
 			df = pd.read_table(filename, index_col=0)
 		else:
@@ -48,21 +51,38 @@ def main():
 			all_group_id_list = all_group_id_list + group_id_list
 
 		single_elution_df = pd.DataFrame(df[args.spectral_count_type])
-		single_elution_df.columns = [fraction_name]
+
+                if args.remove_zero_unique:
+                    single_elution_df = single_elution_df[single_elution_df['Unique'] > 0]
+
+                if args.fraction_name_from_filename:
+                    fraction_name = os.path.basename(filename).split('.')[0]
+                    print fraction_name
+
+                    single_elution_df.columns = [fraction_name]
+                else:
+                    single_elution_df.columns = [i]
+
 		#print single_elution_df
 		elution_profile_df = pd.concat([elution_profile_df, single_elution_df], axis=1)
 
 
 	elution_profile_df = elution_profile_df.fillna(0)
 
-	if args.add_total_count:
-		total_count = pd.DataFrame(elution_profile_df.sum(axis=1))
-		total_count.columns = ['TotalCount']
-		elution_profile_df = pd.concat([total_count, elution_profile_df], axis=1)
+	if args.msblender_format:
+            total_count = pd.DataFrame(elution_profile_df.sum(axis=1))
+            total_count.columns = ['TotalCount']
+            elution_profile_df = pd.concat([total_count, elution_profile_df], axis=1)
+            elution_profile_df.index.name='#ProtID'
 
-	elution_profile_df.to_csv(args.output_filename, sep='\t')
+        if args.parse_uniprot_id:
+            uniprots = [x.split('|')[1] if len(x.split('|'))>1 else x for x in elution_profile_df.index ]
+            elution_profile_df.index = uniprots
+
+        elution_profile_df.to_csv(args.output_filename, sep='\t')
 
 	if args.group:
+                #kdrew: audit the group id list
 		print "all_group_id_list"
 		print len(all_group_id_list)
 		group_id_set = set([tuple(x) for x in all_group_id_list])
