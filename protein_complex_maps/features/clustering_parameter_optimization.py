b4c604b5c6877fed91b608024abeff663f3e3747
Kevin Drew <kdrew@utexas.edu>
2015-11-28 17:24:42 -0600

    Refactored parameter input to be in a dictionary, stored ppis with scores in a dict for later retreival, started two stage clustering code (still testing)

--- protein_complex_maps/features/clustering_parameter_optimization.py
+++ protein_complex_maps/features/clustering_parameter_optimization.py
@@ -45,6 +45,12 @@ def main():
     parser.add_argument("--ppi_fraction", action="store", dest="ppi_fraction", nargs='+', required=False, 
                                     default=[0.005,0.01,0.05,.1,.25,.5,.75,1.0], 
                                     help="Use top fraction for further clustering, default = 0.005 0.01 0.05 .1 .25 .5 .75 1.0")
+    parser.add_argument("--mcl", action="store", dest="mcl_bin", required=False, 
+                                    default='mcl',
+                                    help="Location of mcl binary, default = 'mcl' ")
+    parser.add_argument("--mcl_inflation", action="store", dest="mcl_inflation", nargs='+', required=False, 
+                                    default=[None],
+                                    help="MCL Inflation (-I) parameter, default = [None] (no 2-stage clustering), docs suggest = 1.2 - 5.0")
     parser.add_argument("--eval_metric", action="store", dest="eval_metric", required=False, default='mmr',
                                     help="Evaluation metric used to determine best set of parameters (mmr, acc, sensitivity, ppv), default=mmr")
     parser.add_argument("--procs", action="store", type=int, dest="procs", required=False, default=1,
@@ -64,9 +70,12 @@ def main():
 
     #kdrew: read in the input network into a string
     with open (args.input_network, "r") as input_network_file:
-        #data=input_network_file.read().replace('\n', '')
-        #input_network_str = input_network_file.read()
         input_network_list = input_network_file.readlines()
+
+
+    ppi_scores = dict()
+    for ppi in input_network_list:
+        ppi_scores[frozenset([ppi.split()[0],ppi.split()[1]])] = float(ppi.split()[2])
 
     random.seed(args.random_seed)
     #for i in range(args.bootstrap_iter):
@@ -82,40 +91,56 @@ def main():
     seed_method_sweep = args.clusterone_seed_method
     density_sweep = args.clusterone_density
     fraction_sweep = args.ppi_fraction
+    inflation_sweep = args.mcl_inflation
 
     best_size = None
     best_density = None
     best_fraction = None
     best_overlap = None
     best_seed_method = None
+    best_inflation = None
     best_eval = None
 
     p = mp.Pool(args.procs)
     network_input_list = []
-    for ii, parameters  in enumerate(it.product(size_sweep, density_sweep, fraction_sweep, overlap_sweep, seed_method_sweep )):
+    for ii, parameters  in enumerate(it.product(size_sweep, density_sweep, fraction_sweep, overlap_sweep, seed_method_sweep, inflation_sweep )):
+        print parameters
         #kdrew: unpack parameters
-        size, density, fraction, overlap, seed_method = parameters
+        size, density, fraction, overlap, seed_method, inflation  = parameters
 
         #kdrew: only take the topN ppis, assumes already sorted network (probably stupidly)
         sizeOfTopNetwork = int(len(input_network_list)*float(fraction))
         network_list = input_network_list[0:sizeOfTopNetwork]
 
         #kdrew: cluster network_list
-        parameter_input = (network_list, args, str(size), str(density), str(overlap), str(seed_method), str(fraction), ii)
+        parameter_dict = dict()
+        parameter_dict['network_list'] = network_list
+        parameter_dict['ppi_scores'] = ppi_scores
+        parameter_dict['args'] = args
+        parameter_dict['size'] = str(size)
+        parameter_dict['density'] = str(density)
+        parameter_dict['overlap'] = str(overlap)
+        parameter_dict['seed_method'] = str(seed_method)
+        parameter_dict['fraction'] = str(fraction)
+        parameter_dict['inflation'] = str(inflation)
+        parameter_dict['i'] = ii
+
         #kdrew: append to list of parameters for input into multiprocessor map
-        network_input_list.append(parameter_input)
+        network_input_list.append(parameter_dict)
 
     #kdrew: call clustering on parameter combinations
     cluster_predictions = p.map(cluster_helper, network_input_list)
 
     #kdrew: iterate through clustering results, returns tuple of prediction and number of iteration (ii)
     for cluster_prediction, ii in cluster_predictions:
-        network_list = network_input_list[ii][0]
-        size = network_input_list[ii][2] 
-        density = network_input_list[ii][3] 
-        overlap = network_input_list[ii][4] 
-        seed_method = network_input_list[ii][5] 
-        fraction = network_input_list[ii][6] 
+
+        network_list = network_input_list[ii]['network_list']
+        size = network_input_list[ii]['size']
+        density = network_input_list[ii]['density']
+        overlap = network_input_list[ii]['overlap']
+        seed_method = network_input_list[ii]['seed_method']
+        fraction = network_input_list[ii]['fraction']
+        inflation = network_input_list[ii]['inflation']
 
         #kdrew: compare gold standard vs predicted clusters
         cplx_comparison = cc.ComplexComparison(gold_standard_complexes, cluster_prediction) 
@@ -125,7 +150,7 @@ def main():
         metric_dict['sensitivity'] = cplx_comparison.sensitivity() 
         metric_dict['ppv'] = cplx_comparison.ppv() 
         metric_dict['mmr'] = cplx_comparison.mmr() 
-        print "size %s, density %s, overlap %s, seed_method %s, fraction %s, acc %s, sensitivity %s, ppv %s, mmr %s" % (size, density, overlap, seed_method, fraction, metric_dict['acc'], metric_dict['sensitivity'], metric_dict['ppv'], metric_dict['mmr'])
+        print "size %s, density %s, overlap %s, seed_method %s, fraction %s, inflation %s, acc %s, sensitivity %s, ppv %s, mmr %s" % (size, density, overlap, seed_method, fraction, inflation, metric_dict['acc'], metric_dict['sensitivity'], metric_dict['ppv'], metric_dict['mmr'])
 
 
 
@@ -138,14 +163,29 @@ def main():
         bootstrapped_networks = [ sn[:sizeOfBootstrap] for sn in shuffled_networks ]
         bootstrapped_test_networks = [ sn[sizeOfBootstrap:] for sn in shuffled_networks ]
 
-        multiproc_input = [(boot_net, args, str(size), str(density), str(overlap), str(seed_method), str(fraction), i) for i, boot_net in enumerate(bootstrapped_networks)]
+        #multiproc_input = [(boot_net, args, str(size), str(density), str(overlap), str(seed_method), str(fraction), i) for i, boot_net in enumerate(bootstrapped_networks)]
+        multiproc_input = []
+
+        for i, boot_net in enumerate(bootstrapped_networks):
+            parameter_dict = dict()
+            parameter_dict['network_list'] = boot_net
+            parameter_dict['args'] = args
+            parameter_dict['size'] = str(size)
+            parameter_dict['density'] = str(density)
+            parameter_dict['overlap'] = str(overlap)
+            parameter_dict['seed_method'] = str(seed_method)
+            parameter_dict['fraction'] = str(fraction)
+            parameter_dict['inflation'] = str(inflation)
+            parameter_dict['i'] = i
+            multiproc_input.append(parameter_dict)
+
         bootstrapped_cluster_predictions = p.map(cluster_helper, multiproc_input)
 
         #kdrew: compare full clustered set vs bootstrapped clusters
         multiproc_input = [(cluster_prediction, predicted_clusters, bootstrapped_test_networks[i]) for predicted_clusters, i in bootstrapped_cluster_predictions]
         bootstrap_cplx_cmp_metrics = p.map(comparison_helper, multiproc_input) 
         for boot_cmp in bootstrap_cplx_cmp_metrics:
-            print "bootstrapped: size %s, density %s, overlap %s, seed_method %s, fraction %s, acc %s, sensitivity %s, ppv %s, mmr %s, ppi_recovered %s" % (size, density, overlap, seed_method, fraction, boot_cmp['acc'], boot_cmp['sensitivity'], boot_cmp['ppv'], boot_cmp['mmr'], boot_cmp['percent_ppi_recovered'])
+            print "bootstrapped: size %s, density %s, overlap %s, seed_method %s, fraction %s, inflation %s,  acc %s, sensitivity %s, ppv %s, mmr %s, ppi_recovered %s" % (size, density, overlap, seed_method, fraction, inflation, boot_cmp['acc'], boot_cmp['sensitivity'], boot_cmp['ppv'], boot_cmp['mmr'], boot_cmp['percent_ppi_recovered'])
 
 
         #kdrew: keeping track of the best parameter set
@@ -156,8 +196,9 @@ def main():
             best_overlap = overlap
             best_seed_method = seed_method
             best_fraction = fraction
+            best_inflation = inflation
             best_cluster_prediction = cluster_prediction
-            print "best size: %s density: %s overlap: %s seed_method: %s fraction: %s numOfClusters: %s" % (best_size, best_density, best_overlap, best_seed_method, best_fraction, len(best_cluster_prediction))
+            print "best size: %s density: %s overlap: %s seed_method: %s fraction: %s inflation: %s numOfClusters: %s" % (best_size, best_density, best_overlap, best_seed_method, best_fraction, best_inflation, len(best_cluster_prediction))
 
 
 
@@ -212,15 +253,18 @@ def comparison_helper(parameter_tuple):
 
     return d
 
-def cluster_helper(parameter_tuple):
-    input_network_list = parameter_tuple[0]
-    args = parameter_tuple[1]
-    size = parameter_tuple[2]
-    density = parameter_tuple[3]
-    overlap = parameter_tuple[4]
-    seed_method = parameter_tuple[5]
-    fraction = parameter_tuple[6]
-    i = parameter_tuple[7]
+def cluster_helper(parameter_dict):
+
+    input_network_list = parameter_dict['network_list']
+    ppi_scores = parameter_dict['ppi_scores']
+    args = parameter_dict['args']
+    size = parameter_dict['size']
+    density = parameter_dict['density']
+    overlap = parameter_dict['overlap']
+    seed_method = parameter_dict['seed_method']
+    fraction = parameter_dict['fraction']
+    i = parameter_dict['i']
+    inflation = parameter_dict['inflation']
 
     #kdrew: create temp file for bootstrapped input network, clusterone requires a file input
     fileTemp = tf.NamedTemporaryFile(delete=False)
@@ -243,6 +287,34 @@ def cluster_helper(parameter_tuple):
     for line in clustone_out.split('\n'):
         if len(line.split() ) > 0:
             predicted_clusters.append(line.split())
+
+    if inflation != None:
+        #kdrew: for each predicted cluster, recluster using MCL
+        for clust in predicted_clusters:
+            #kdrew: for every pair in cluster find edge weight in input_network_list(?)
+            fileTemp = tf.NamedTemporaryFile(delete=False)
+            outTemp = tf.NamedTemporaryFile(delete=False)
+            try:
+                for prot1, prot2 in it.combinations(clust,2):
+                    try:
+                        ppi_score = ppi_scores[frozenset([prot1,prot2])] 
+                    except KeyError:
+                        ppi_score = 0.0
+
+                    ppi_str = "%s\t%s\t%s" % (prot1, prot2, ppi_score)
+                    fileTemp.write(ppi_str)
+                fileTemp.close()
+
+                proc = sp.Popen([args.mcl_bin, fileTemp.name, '-abc', '-o', outTemp.name, '-I', inflation], stdout=sp.PIPE, stderr=sp.PIPE)
+                mcl_out, err = proc.communicate()
+
+                print mcl_out
+
+
+            finally:
+                os.remove(fileTemp.name)
+                os.remove(outTemp.name)
+
 
 
     return predicted_clusters, i
