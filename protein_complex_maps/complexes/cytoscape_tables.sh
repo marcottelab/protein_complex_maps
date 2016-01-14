02dad0527a654eb955c8d71297be7456d7083db7
Kevin Drew <kdrew@utexas.edu>
2016-01-14 17:33:01 -0600

    Utility bash script to generate cytoscape files for complex maps
    Network, edge attributes with svm score and node attributes with acc,
    genename, uniprot url, etc

---
+++ protein_complex_maps/complexes/cytoscape_tables.sh
@@ -0,0 +1,13 @@
+
+echo $1 #cluster file
+echo $2 #pairwise file
+
+#kdrew: convert clusters to pairwise with cluster ids attached
+python ~/scripts/protein_complex_maps/protein_complex_maps/util/cluster2pairwise.py --filename $1 --output_filename ${1/.txt/.pairsWclustID.txt} --add_cluster_id
+
+#kdrew: generate node attribute table
+python ~/scripts/protein_complex_maps/protein_complex_maps/util/cluster2node_table.py --cluster_filename $1 --from_id P_ENTREZGENEID --reviewed --output_filename ${1/.txt/.nodeTable.txt}
+
+#kdrew: generate edge attribute table
+python ~/scripts/protein_complex_maps/protein_complex_maps/util/pairwise2clusterid.py --pairwise_filename $2 --cluster_filename $1 --output_filename ${1/.txt/.edgeAttributeWClusterid.txt}
+
