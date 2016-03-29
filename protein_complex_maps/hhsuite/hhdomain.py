51f65fef9792dc9914c66eae1aceceb44a3572a8
Kevin Drew <kdrew@utexas.edu>
2016-03-29 10:31:06 -0500

    Updated main function to output domain lists per protein to an outfile
    Also, allows id conversion

--- protein_complex_maps/hhsuite/hhdomain.py
+++ protein_complex_maps/hhsuite/hhdomain.py
@@ -2,7 +2,10 @@
 import argparse
 import numpy as np
 
+from os.path import basename
+
 import protein_complex_maps.hhsuite.hhresults_parser as hrp
+import protein_complex_maps.protein_util as pu
 
 def main():
 
@@ -11,19 +14,53 @@ def main():
                                             help="Filenames of hhsuite results")
     parser.add_argument("--prob_threshold", action="store", type=float, dest="prob_threshold", required=False, default=90.0,
                                             help="Only consider domain hits above probability threshold (note: hhsuite prob is x100), default = 90.0")
+    parser.add_argument("--parse_uniprot_from_filename", action="store_true", dest="parse_uniprot_from_filename", required=False, default=False,
+                                            help="Parse uniprot id from hhr filename")
+    parser.add_argument("--output_filename", action="store", dest="output_filename", required=True, 
+                                            help="Filename to write output to")
+
+    parser.add_argument("--convert_id", action="store_true", dest="convert_id", required=False, default=False,
+                                            help="Convert id from one identifier to another")
+    parser.add_argument("--to_id", action="store", dest="to_id", required=False, default="ACC", 
+                                            help="convert ids to this type, (P_ENTREZGENEID,ACC,etc), default=ACC")
+    parser.add_argument("--reviewed", action="store_true", dest="reviewed", required=False, default=False,
+                                            help="map only to reviewed ids, default=False")
     args = parser.parse_args()
 
+    
 
+    domain_list_dict = dict()
     for hhr_filename in args.hhr_filenames:
-
-        print hhr_filename
-
         greedy_list = greedy_domain_map(hhr_filename, args.prob_threshold)
+        domain_list_dict[hhr_filename] = [x['hit'].split()[1] for x in greedy_list]
 
-        for greedy_result in greedy_list:
-            print "%s : %s-%s : %s" % (greedy_result['hit'], greedy_result['query_hmm_begin'], greedy_result['query_hmm_end'], greedy_result['prob'] )
+    uniprot_domain_list_dict = dict()
+    if args.parse_uniprot_from_filename:
+        for hhr_filename in domain_list_dict:
+            uniprot_id = basename(hhr_filename).split('_')[1]
+            uniprot_domain_list_dict[uniprot_id] = domain_list_dict[hhr_filename]
+        domain_list_dict = uniprot_domain_list_dict
 
-        print ""
+        if args.convert_id:
+            ACC2outputID_map = pu.map_protein_ids(uniprot_domain_list_dict.keys(), "ACC", args.to_id, reviewed=args.reviewed)
+            convert_id_domain_list_dict = dict()
+            for uniprot_id in uniprot_domain_list_dict:
+                try:
+                    converted_id = ACC2outputID_map[uniprot_id][0]
+                    convert_id_domain_list_dict[converted_id] = uniprot_domain_list_dict[uniprot_id]
+                except KeyError:
+                    print "cannot find uniprot_id: %s" % (uniprot_id)
+                    convert_id_domain_list_dict[uniprot_id] = uniprot_domain_list_dict[uniprot_id]
+
+            domain_list_dict = convert_id_domain_list_dict
+
+    fout = open(args.output_filename,"wb")
+    for protid in domain_list_dict:
+        out_str = "%s,%s\n" % (protid,','.join(domain_list_dict[protid]))
+        fout.write(out_str)
+
+    fout.close()
+
 
 
 def greedy_domain_map(hhr_filename, prob_threshold):
