5c54030dc54d0d99fd8f5e42e69ba1a61e516099
Kevin Drew <kdrew@utexas.edu>
2016-04-04 16:52:08 -0500

    Added ability to plot cumulative precision and recall.  Also can change marker size based on the number of clusters used for each clique bin

--- protein_complex_maps/plots/plot_prcliquesize_trajectories.py
+++ protein_complex_maps/plots/plot_prcliquesize_trajectories.py
@@ -31,6 +31,10 @@ def main():
                         help="Names of clustering, same order as pr_table, to be used for plotting legends")
     parser.add_argument("--plot_filename", action="store", dest="plot_filename", required=False, default=None,
                         help="Filename of output plot")
+    parser.add_argument("--cumulative_pr", action="store_true", dest="cumulative_pr", required=False, default=False,
+                        help="Use cumulative precision and recall for plotting")
+    parser.add_argument("--plot_numOfClusters", action="store_true", dest="plot_numOfClusters", required=False, default=False,
+                        help="Plot the number of clusters that contributed to given clique size as size of plotted dot")
     args = parser.parse_args()
 
 
@@ -38,11 +42,11 @@ def main():
 
     #colors = ['#FF7900','660000','DDB505','C2120B']
 
-    plot_prcliquesize_trajectories(pr_dict, pr_dict['recall'].keys(), plot_filename = args.plot_filename)
+    plot_prcliquesize_trajectories(pr_dict, pr_dict['recall'].keys(), plot_filename = args.plot_filename, cumulative=args.cumulative_pr, plot_numOfClusters=args.plot_numOfClusters)
 
 
 
-def plot_prcliquesize_trajectories(pr_dict, psweep_indices, pr_dict2=None, plot_filename=None, colors=None):
+def plot_prcliquesize_trajectories(pr_dict, psweep_indices, pr_dict2=None, plot_filename=None, colors=None, cumulative=False, plot_numOfClusters=False):
     
     plot_data = []
 
@@ -51,25 +55,37 @@ def plot_prcliquesize_trajectories(pr_dict, psweep_indices, pr_dict2=None, plot_
 
 
     for ii in psweep_indices:
-        precision_list = pr_dict['precision'][ii]
-        recall_list = pr_dict['recall'][ii]
+        numOfClusters_list = pr_dict['numOfClusters'][ii]
+
+        if cumulative:
+            precision_list = pr_dict['cumulative_precision'][ii]
+            recall_list = pr_dict['cumulative_recall'][ii]
+        else:
+            precision_list = pr_dict['precision'][ii]
+            recall_list = pr_dict['recall'][ii]
 
         if colors != None:
             line, = plt.plot(recall_list, precision_list, '.-', linewidth=1, label=ii, color=colors[ii])
         else:
-            line, = plt.plot(recall_list, precision_list, '.-', linewidth=1, label=ii)
+            line, = plt.plot(recall_list, precision_list, '.-', linewidth=1, label=ii )
 
         for i, xy in enumerate(zip(recall_list, precision_list)):
             cliquesize = i+2
             ax.annotate('%s' % cliquesize, xy=xy, textcoords='offset points', fontsize=8) 
+            if plot_numOfClusters:
+                dot, = plt.plot([recall_list[i]], [precision_list[i]], '.', markersize=np.log(numOfClusters_list[i])*10, color=line.get_color())
 
 
     ax.set_ylim([ax.get_ylim()[0]-0.01,ax.get_ylim()[1]+0.01])
     ax.set_xlim([ax.get_xlim()[0]-0.01,ax.get_xlim()[1]+0.01])
 
     plt.legend(loc="best", fontsize=8)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
+    if cumulative:
+        plt.xlabel('Cumulative Recall')
+        plt.ylabel('Cumulative Precision')
+    else:
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
 
     plt.savefig(plot_filename)
 
