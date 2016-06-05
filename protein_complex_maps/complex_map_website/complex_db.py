574a257c5b337edeb3d18a0b8f42c67550454bf3
Kevin Drew <kdrew@utexas.edu>
2016-06-04 23:27:05 -0500

    Updated/Improved website: Added enrichments, complex view Added bootstrap css and js files Script to load enrichments into database Added wsgi to run through apache

--- protein_complex_maps/complex_map_website/complex_db.py
+++ protein_complex_maps/complex_map_website/complex_db.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -32,6 +33,11 @@ class Complex(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     complex_id = db.Column(db.Integer, unique=True)
     proteins = db.relationship('Protein', secondary='protein_complex_mapping', back_populates='complexes', lazy='dynamic')
+    enrichments = db.relationship('ComplexEnrichment', lazy='dynamic')
+
+    def complex_link(self,):
+        retstr = "<a href=displayComplexes?complex_key=%s>%s</a>" % (self.complex_id, self.complex_id)
+        return retstr
 
 class Protein(db.Model):
     """A single protein"""
@@ -43,10 +49,42 @@ class Protein(db.Model):
     uniprot_url = db.Column(db.String(255))
     complexes = db.relationship('Complex', secondary='protein_complex_mapping',  back_populates='proteins', lazy='dynamic')
 
+    def uniprot_link(self,):
+        retstr = "<a href=%s target=\"_blank\">%s</a>" % (self.uniprot_url, self.genename)
+        return retstr
+
+
 class ProteinComplexMapping(db.Model):
     """A mapping between proteins and complexes"""
-    #id = db.Column(db.Integer, primary_key=True)
     protein_key = db.Column(db.Integer, db.ForeignKey('protein.id'), primary_key=True)
     complex_key = db.Column(db.Integer, db.ForeignKey('complex.id'), primary_key=True)
+
+class ComplexEnrichment(db.Model):
+    """Annotation Enrichment for a Complex"""
+    id = db.Column(db.Integer, primary_key=True)
+    complex_key = db.Column(db.Integer, db.ForeignKey('complex.id'))
+    #  signf   corr. p-value   T   Q   Q&T Q&T/Q   Q&T/T   term ID     t type  t group    t name and depth in group        Q&T list
+    corr_pval = db.Column(db.Float)
+    t_count = db.Column(db.Integer)
+    q_count = db.Column(db.Integer)
+    qandt_count = db.Column(db.Integer)
+    qandt_by_q = db.Column(db.Float)
+    qandt_by_t = db.Column(db.Float)
+    term_id = db.Column(db.String(255))
+    t_type = db.Column(db.String(63))
+    t_group = db.Column(db.Integer)
+    t_name = db.Column(db.String(255))
+    depth_in_group = db.Column(db.Integer)
+    qandt_list = db.Column(db.String(255))
+
+    def get_proteins(self,):
+        proteins = []
+        for acc in self.qandt_list.split(','):
+            prot = db.session.query(Protein).filter_by(uniprot_acc=acc).first()
+            proteins.append(prot)
+        return proteins
+
+
+
 
 
--- protein_complex_maps/complex_map_website/complex_map_website.py
+++ protein_complex_maps/complex_map_website/complex_map_website.py
@@ -1,16 +1,21 @@
 
 import protein_complex_maps.complex_map_website.complex_db as cdb
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import func, or_
 
 db = cdb.get_db()
 app = cdb.get_app()
 
-from flask.ext.wtf import Form
+#from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms.fields import StringField, SubmitField
 
 
 class SearchForm(Form):
     complex_id = StringField(u'Complex ID:')
-    genename = StringField(u'Gene Name:')
+    genename = StringField(u'Gene Name (ex. OFD1):')
+    enrichment = StringField(u'Enrichment (ex. cilium):')
+    protein = StringField(u'Protein (ex. Centrosomal protein):')
     submit = SubmitField(u'Search')
 
 from flask import render_template
@@ -25,14 +30,71 @@ def root(complexes=[]):
 
 @app.route("/displayComplexesForGeneName")
 def displayComplexesForGeneName():
-    print "in displayComplexes"
     genename = request.args.get('genename')
     form = SearchForm()
-
-    protein = db.session.query(cdb.Protein).filter_by(genename=genename).one()
-    complexes = protein.complexes.all()
+    #kdrew: do error checking
+    try:
+        protein = db.session.query(cdb.Protein).filter_by(genename=func.upper(genename)).one()
+        complexes = protein.complexes.all()
+    except NoResultFound:
+        complexes = []
 
     return render_template('index.html', form=form, complexes=complexes)
+
+@app.route("/displayComplexesForEnrichment")
+def displayComplexesForEnrichment():
+    enrichment = request.args.get('enrichment')
+    form = SearchForm()
+    #print enrichment
+    #kdrew: do error checking
+    try:
+        enrichment_complex_keys_query = db.session.query(cdb.ComplexEnrichment.complex_key).filter(
+                    ( cdb.ComplexEnrichment.t_name.like('%'+enrichment+'%')) | (cdb.ComplexEnrichment.term_id.like('%'+enrichment+'%' ) ) )
+        enrichment_complex_keys = enrichment_complex_keys_query.all()
+        enrichment_complex_keys_set = set([x[0] for x in enrichment_complex_keys])
+        if len(enrichment_complex_keys_set) == 0:
+            complexes = []
+        else:
+            complexes = db.session.query(cdb.Complex).filter(cdb.Complex.id.in_(enrichment_complex_keys_set)).all()
+    except NoResultFound:
+        print "NoResultFound"
+        complexes = []
+
+    return render_template('index.html', form=form, complexes=complexes)
+
+@app.route("/displayComplexesForProtein")
+def displayComplexesForProtein():
+    protein_search = request.args.get('protein')
+    form = SearchForm()
+    #print protein
+    #kdrew: do error checking
+    complexes = []
+    try:
+        proteins = db.session.query(cdb.Protein).filter(cdb.Protein.proteinname.like('%'+protein_search+'%')).all()
+        for protein in proteins:
+            print protein
+            complexes = complexes + protein.complexes.all()
+
+        #kdrew: remove redudant complexes
+        complexes = list(set(complexes))
+
+    except NoResultFound:
+        print "NoResultFound"
+        complexes = []
+
+    return render_template('index.html', form=form, complexes=complexes)
+
+@app.route("/displayComplexes")
+def displayComplexes():
+    complex_key = request.args.get('complex_key')
+    form = SearchForm()
+    #kdrew: do error checking
+    try:
+        complexes = db.session.query(cdb.Complex).filter_by(complex_id=complex_key).all()
+    except NoResultFound:
+        complexes = []
+
+    return render_template('complex.html', form=form, complexes=complexes)
 
 
 @app.route(u'/search', methods=[u'POST'])
@@ -40,7 +102,13 @@ def searchComplexes():
     form = SearchForm()
     complexes = []
     if form.validate_on_submit():
-        return redirect(url_for('displayComplexesForGeneName', genename=form.genename.data))
+        if len(form.genename.data) > 0:
+            return redirect(url_for('displayComplexesForGeneName', genename=form.genename.data))
+        elif len(form.enrichment.data) > 0:
+            return redirect(url_for('displayComplexesForEnrichment', enrichment=form.enrichment.data))
+        elif len(form.protein.data) > 0:
+            return redirect(url_for('displayComplexesForProtein', protein=form.protein.data))
+
 
 
 if __name__ == "__main__":
