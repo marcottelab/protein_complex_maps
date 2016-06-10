985e3567fa48c3fb6ee03303a8c12089793587cc
Kevin Drew <kdrew@utexas.edu>
2016-06-10 08:59:30 -0500

    Added ability to output actual values of columns in feature matrix.
    Very inefficient processing, need to fix

--- protein_complex_maps/util/pairwise2clusterid.py
+++ protein_complex_maps/util/pairwise2clusterid.py
@@ -18,7 +18,11 @@ def main():
     parser.add_argument("--header_names", action="store", nargs='+', dest="header_names", required=False, default=None,
                                     help="Names for header relating to features")
     parser.add_argument("--features", action="store", nargs='+', dest="features", required=False, default=None,
-                                    help="Names of fields for features in input_feature_matrix, will test !=0.0")
+                                    help="Names of fields for features in input_feature_matrix, will test !=0.0 unless --write_value set")
+    parser.add_argument("--write_value", action="store_true", dest="write_value", required=False, default=False,
+                                    help="Output values for features in input_feature_matrix specified by --features")
+    parser.add_argument("--id_columns", action="store", nargs='+', dest="id_columns", required=False, default=None,
+                                    help="Names of columns for ids in input_feature_matrix")
     parser.add_argument("--output_filename", action="store", dest="out_filename", required=True,
                                             help="Output filename ")
     args = parser.parse_args()
@@ -40,7 +44,15 @@ def main():
 
     if args.feature_matrix != None:
         feature_table = pd.read_csv(args.feature_matrix,sep=args.sep)
-        fout.write("id1\tscore\t%s\n" % "\t".join(args.header_names))
+        if 'frozenset_ids_str_order' not in feature_table.columns:
+            #kdrew: create frozenset_ids_str_order column
+            feature_table['frozenset_ids'] = map(frozenset,feature_table[[args.id_columns[0],args.id_columns[1]]].values)
+            feature_table['frozenset_ids_str_order'] = feature_table['frozenset_ids'].apply(list).apply(sorted).apply(str)
+
+        if args.header_names != None:
+            fout.write("id1\tscore\t%s\n" % "\t".join(args.header_names))
+        else:
+            fout.write("id1\tscore\t%s\n" % "\t".join(args.features))
     else:
         fout.write("id1\tscore\n")
 
@@ -56,8 +68,11 @@ def main():
                 out_list = []
                 ids_str_order = "%s" % sorted([prot_pair[0],prot_pair[1]])
                 for field in args.features:
-                    #kdrew: seems inefficent 
-                    out_list.append(str(feature_table[feature_table['frozenset_ids_str_order'] == ids_str_order][field].values[0] != 0.0))
+                    if args.write_value:
+                        out_list.append(str(feature_table[feature_table['frozenset_ids_str_order'] == ids_str_order][field].values[0]))
+                    else:
+                        #kdrew: seems inefficent 
+                        out_list.append(str(feature_table[feature_table['frozenset_ids_str_order'] == ids_str_order][field].values[0] != 0.0))
 
                 fout.write("%s (pp) %s\t%s\t%s" % (id1, id2, score, "\t".join(out_list)))
             else:
