dabc79579417f9bd6d8f2ea4ff6c9ec8bb96b003
Kevin Drew <kdrew@utexas.edu>
2016-07-18 22:13:26 -0500

    Added Edge and Evidence classes and tables

--- protein_complex_maps/complex_map_website/complex_db.py
+++ protein_complex_maps/complex_map_website/complex_db.py
@@ -2,6 +2,7 @@
 from flask import Flask
 #from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func, or_, and_
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -39,6 +40,24 @@ class Complex(db.Model):
         retstr = "<a href=displayComplexes?complex_key=%s>%s</a>" % (self.complex_id, self.complex_id)
         return retstr
 
+    def edges(self,):
+        es = []
+        pairs = set()
+        for prot1 in self.proteins:
+            for prot2 in self.proteins:
+                #kdrew: check to see if the pair has already been entered
+                if frozenset((prot1,prot2)) not in pairs:
+                    edge = db.session.query(Edge).filter((and_(Edge.protein_key == prot1.id, Edge.protein_key2 == prot2.id) | and_(Edge.protein_key == prot2.id,Edge.protein_key2 == prot1.id))).first()
+                    #es = es + edge
+                    if edge != None:
+                        es.append(edge)
+                        #kdrew: add pair of prot ids to keep track of edges already added
+                        pairs.add(frozenset((prot1,prot2)))
+
+        return sorted(list(set(es)), key=lambda es: es.score, reverse=True)
+
+        
+
 class Protein(db.Model):
     """A single protein"""
     id = db.Column(db.Integer, primary_key=True)
@@ -52,6 +71,26 @@ class Protein(db.Model):
     def uniprot_link(self,):
         retstr = "<a href=%s target=\"_blank\">%s</a>" % (self.uniprot_url, self.genename)
         return retstr
+
+class Edge(db.Model):
+    """A protein protein edge"""
+    id = db.Column(db.Integer, primary_key=True)
+    protein_key = db.Column(db.Integer, db.ForeignKey('protein.id') )
+    protein_key2 = db.Column(db.Integer, db.ForeignKey('protein.id') )
+    score = db.Column(db.Float)
+
+    evidences = db.relationship('Evidence', lazy='dynamic')
+
+    def get_proteins(self,):
+        prot1 = db.session.query(Protein).filter(Protein.id==self.protein_key).first()
+        prot2 = db.session.query(Protein).filter(Protein.id==self.protein_key2).first()
+        return (prot1, prot2)
+
+class Evidence(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    edge_key = db.Column(db.Integer, db.ForeignKey('edge.id'))
+    evidence_type = db.Column(db.String(255))
+    
 
 
 class ProteinComplexMapping(db.Model):
