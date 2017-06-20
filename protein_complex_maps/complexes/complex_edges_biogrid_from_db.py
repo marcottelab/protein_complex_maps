392175a21d66581b54057dfb584945b7aaa2277e
Kevin Drew <kdrew@utexas.edu>
2017-06-20 17:17:23 -0500

    Updated biogrid formatter script, complex_edges_biogrid_from_db, to include genenames and pubmed id

--- protein_complex_maps/complexes/complex_edges_biogrid_from_db.py
+++ protein_complex_maps/complexes/complex_edges_biogrid_from_db.py
@@ -21,6 +21,8 @@ def main():
                                     help="delimiter within fields, default = \\t")
     parser.add_argument("--taxon", action="store", dest="taxon", required=False, default=9606,
                                     help="Global taxon of protein interactions, default = 9606")
+    parser.add_argument("--pubmedid", action="store", dest="pubmedid", required=False, default='28596423',
+                                    help="PubMedID for publication, default = 28596423 (Drew et al. 2017 MolSysBio)")
 
 
     args = parser.parse_args()
@@ -34,7 +36,7 @@ def main():
 
     proteins = set()
     protein_edges = set()
-    output_str = "InteractorA%sInteractorB%sInteractorA_taxon%sInteractorB_taxon%sEvidence_Code%sPubmedID%sScore%sModification%sPhenotypes%sComment_Qualifier\n" % tuple([args.field_delimiter]*9)
+    output_str = "InteractorA%sInteractorB%sInteractorA_genename%sInteractorB_genename%sInteractorA_taxon%sInteractorB_taxon%sEvidence_Code%sPubmedID%sScore%sModification%sPhenotypes%sComment_Qualifier\n" % tuple([args.field_delimiter]*11)
 
     for edge in edges:
         prots = edge.get_proteins()
@@ -87,13 +89,15 @@ def main():
         if len(PMID_set) >0:
             pmid_str = "(PMIDs: %s)" % (' '.join(list(PMID_set))) 
 
-        output_str = output_str + args.field_delimiter.join(["%s"]*10) % (
+        output_str = output_str + args.field_delimiter.join(["%s"]*12) % (
                                                             prots[0].gene_id,
                                                             prots[1].gene_id,
+                                                            prots[0].genename(),
+                                                            prots[1].genename(),
                                                             args.taxon,
                                                             args.taxon,
                                                             args.inner_field_delimiter.join(evidence_codes),
-                                                            "",  #kdrew: PubmedID
+                                                            args.pubmedid,  #kdrew: PubmedID
                                                             edge.score, 
                                                             "", #kdrew: Modifications
                                                             "", #kdrew: Phenotypes
