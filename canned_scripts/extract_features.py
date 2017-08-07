ce8c234fc596742e471917953ad2c018401fc5cf
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2017-08-07 10:16:05 -0500

    Added thresholding.
    Still needs work to avoid hardcoding

--- canned_scripts/extract_features.py
+++ canned_scripts/extract_features.py
@@ -8,10 +8,12 @@ parser.add_argument("infile")
 parser.add_argument("-f", "--feature", default="pearsonR",choices=ef.available_features)
 parser.add_argument("-r", "--resampling", default=None, choices=ef.resampling_strategies)
 parser.add_argument("-i", "--iterations", default=None, type=int)
+parser.add_argument("-t", "--threshold", default=None, type=int)
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    outfile = "_".join([args.infile.split(".")[0], args.feature, args.resampling, str(args.iterations)+"reps"]) + ".feat"
+    ## This won't work if threshold or iterations are None. Fix
+    outfile = "_".join([args.infile.split(".")[0], args.feature, args.resampling, str(args.iterations)+"reps", "thresh"+str(args.threshold)]) + ".feat"
     elution = ef()
     elution.load(args.infile)
     feature_matrix = elution.extract_features(feature=args.feature,resampling=args.resampling,iterations=args.iterations)
