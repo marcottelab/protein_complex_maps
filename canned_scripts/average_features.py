4eb5a0aea4a74b3bacabfff36ef7a038103f77e0
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2017-09-09 10:10:35 -0500

    Added flag for medians, different printing

--- canned_scripts/average_features.py
+++ canned_scripts/average_features.py
@@ -6,6 +6,7 @@ import pandas as pd
 parser = argparse.ArgumentParser(description="Average a list of feature files")
 parser.add_argument("infiles", nargs="+",help='Feature files. Optionally takes a wildcard like *.csv')
 parser.add_argument("outfile",help="Name of file to write to")
+parser.add_argument("--average_type", default="mean", choices=["mean","median"], help="Choose mean or median values")
 parser.add_argument("--retain_columns",action='store_true',help="Retain the input columns in the ouput")
 parser.add_argument("--log",action='store_true',help="Write log of inputs")
 args = parser.parse_args()
@@ -13,9 +14,9 @@ args = parser.parse_args()
 if __name__ == '__main__':
 
     files_processed = []
-    count = 0
     is_first = True
     for ind,f in enumerate(args.infiles):
+        print "Reading in {}".format(f)
         df = pd.read_csv(f)
         feature = df.columns[2]
         df.columns = ["ID1","ID2","file"+str(ind)]
@@ -25,10 +26,12 @@ if __name__ == '__main__':
             continue
         merged = merged.merge(df,on=["ID1","ID2"],how='outer')
         files_processed.append(f)
-        count += 1
-        print count
 
-    merged["average_{}".format(feature)] = merged.iloc[:,2:].mean(axis=1)
+    if args.average_type == "mean":
+        merged["mean_{}".format(feature)] = merged.iloc[:,2:].mean(axis=1)
+    else:
+        merged["median_{}".format(feature)] = merged.iloc[:,2:].median(axis=1)
+        
     if args.retain_columns == False:
         drop_cols = [col for col in merged.columns if "file" in col]
         merged.drop(drop_cols,axis=1,inplace=True)
