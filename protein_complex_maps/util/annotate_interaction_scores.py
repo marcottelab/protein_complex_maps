2ba48bd9612eaca11c3c1b86ee9f21f00e1098d9
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2017-09-14 11:27:05 -0500

    Add two util scripts

---
+++ protein_complex_maps/util/annotate_interaction_scores.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import argparse
+import pandas as pd
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="With IDs in first two columns, retrieve annotations for each ID")
+    parser.add_argument("--input_pairs", action="store", dest="input_pairs", required=True, 
+                                    help="Filename of input pairs, (first two columns IDs) No header")
+    parser.add_argument("--sep", action="store", dest="sep", required=False, default=' ',
+                                    help="Separator for reading feature csv, default=$")
+    parser.add_argument("--output_file", action="store", dest="output_file", required=True, 
+                                    help="Filename of output file")
+    parser.add_argument("--input_annots", action="store", dest="input_annots", required=True, 
+                                    help="Filename of annotation file")
+    parser.add_argument("--annot_sep", action="store", dest="annot_sep", required=False, default=',', 
+                                    help="Sep of annotation file")
+    parser.add_argument("--annot_key", action="store", dest="annot_key", required=False, default='ID', 
+                                    help="Col name in annotation file that matches first two rows of input file")
+
+    parser.add_argument('-t','--target_col', action="store", dest="target_col",  required=False, default=None, help="designate a specific column to full from annotation file")
+
+
+    args = parser.parse_args()
+
+    pairs = pd.read_csv(args.input_pairs,sep=args.sep, header=None)
+    annots = pd.read_csv(args.input_annots,sep=args.annot_sep)
+
+
+    if args.target_col:
+       annots = annots[[args.annot_key, args.target_col]]
+
+    pairs=pairs.rename(columns = {0:'ID1'})
+    pairs=pairs.rename(columns = {1:'ID2'})
+  
+    pairs = pd.merge(pairs, annots, left_on='ID1', right_on='ID', how='left')    
+
+    pairs = pd.merge(pairs, annots, left_on='ID2', right_on='ID', how='left', suffixes=['_1','_2'])    
+   
+    pairs=pairs.drop('ID_1', axis=1)
+  
+    pairs=pairs.drop('ID_2', axis=1)
+
+    pairs.to_csv(args.output_file,sep=" ",index=False,header=True)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
