cf3396008a366dc2197b163aeebe4376ee2a4489
Kevin Drew <kdrew@utexas.edu>
2017-09-20 18:15:51 -0500

    Refactored to use multiprocessor library
    Created a helper function to run shared bait feature on individual ids
    
    Updated how transformation of pvalue is done.  Now transforms so
    smallest exponent is at the limit of the machine precision

--- protein_complex_maps/features/shared_bait_feature.py
+++ protein_complex_maps/features/shared_bait_feature.py
@@ -7,9 +7,16 @@ import scipy.misc as misc
 import scipy
 import math
 import logging 
+import multiprocessing as mp
 from rpy2.robjects.packages import importr
 from rpy2.robjects.vectors import FloatVector
+from functools import partial
 
+
+#kdrew: this is the smallest exponent before scipy.exp(x) becomes 0.0, empirically found, probably cpu specific
+SMALL_EXPONENT = -745.1332
+#kdrew: this is the largest exponent before scipy.exp(x) becomes inf, empirically found, probably cpu specific
+LARGE_EXPONENT = 709.7827
 
 pd.set_option('display.height', 1000)
 pd.set_option('display.max_rows', 500)
@@ -149,18 +156,22 @@ def main():
                                     help="Use logs to deal with large values when calculating choose, default=False")
     parser.add_argument("--transform_pvalue", action="store_true", dest="transform_pvalue", required=False, default=False,
                                     help="Report a transformed pvalue, default=False")
+    parser.add_argument("--remove_exponents", action="store_true", dest="remove_exponents", required=False, default=False,
+                                    help="Do not report exponents in final output, default=False")
     parser.add_argument("--logname", action="store", dest="logname", required=False, default='shared_bait_feature.log',
                                     help="filename for logging, default=shared_bait_feature.log")
+    parser.add_argument("-j", "--numOfProcs", action="store", type=int, dest="numOfProcs", required=False, default=1,
+                                    help="Number of processers, default=1")
     args = parser.parse_args()
 
     setup_log(args.logname)
 
     feature_table = pd.DataFrame(pd.read_csv(args.feature_matrix, sep=args.sep))
-    output_df = shared_bait_feature(feature_table, args.bait_id_column, args.id_column, args.bh_correct, args.denm, args.logchoose, args.transform_pvalue)
+    output_df = shared_bait_feature(feature_table, args.bait_id_column, args.id_column, args.bh_correct, args.denm, args.logchoose, args.transform_pvalue, numOfProcs=args.numOfProcs, remove_exponents=args.remove_exponents)
     output_df = output_df.sort('neg_ln_pval', ascending=False)
     output_df.to_csv(args.output_file, index=False, header=True)
 
-def shared_bait_feature(feature_table, bait_id_column, id_column, bh_correct=False, denm=False, logchoose=False, transform_pvalue=False):
+def shared_bait_feature(feature_table, bait_id_column, id_column, bh_correct=False, denm=False, logchoose=False, transform_pvalue=False, numOfProcs = 1, remove_exponents=False):
     print(feature_table)
     print(feature_table.columns.values)
     #kdrew: best to enforce ids as strings
@@ -186,85 +197,37 @@ def shared_bait_feature(feature_table, bait_id_column, id_column, bh_correct=Fal
     output_dict2['pval'] = []
     output_dict2['exponents'] = []
 
+
+
+    p = mp.Pool(numOfProcs)
     print("#### printing feature_table_geneid tables ####")
     #kdrew: generating full shared bait table for large datasets is memory intensive, break table down and do one id at a time
-    for geneid in set(feature_table[id_column].values):
-        print("geneid: %s" % geneid)
-        #kdrew: slice feature_table to only have a single id
-        feature_table_geneid = feature_table[feature_table[id_column] == geneid] 
-        #kdrew: join the sliced feature table with the entire feature table on the experiment column (ie. bait_id_column_str)
-        feature_shared_bait_table_geneid = feature_table_geneid.join(feature_table, rsuffix="_right")
-        #print (feature_table_geneid)
+    gene_id_results = p.map(partial(shared_bait_feature_helper, feature_table=feature_table, id_column=id_column, ms_values=ms_values, N=N, denm=denm, logchoose=logchoose, transform_pvalue=transform_pvalue), set(feature_table[id_column].values))
 
-        #kdrew: make ids into strings and generate tuples of pairs of genes
-        feature_shared_bait_table_geneid['gene_id1_str'] = feature_shared_bait_table_geneid[id_column].apply(str)
-        feature_shared_bait_table_geneid['gene_id2_str'] = feature_shared_bait_table_geneid[id_column+'_right'].apply(str)
-        feature_shared_bait_table_geneid['IDs'] = map(sorted, zip(feature_shared_bait_table_geneid['gene_id1_str'].values, feature_shared_bait_table_geneid['gene_id2_str'].values))
-        feature_shared_bait_table_geneid['IDs_tup'] = feature_shared_bait_table_geneid['IDs'].apply(tuple)
-        print (feature_shared_bait_table_geneid)
-
-        #kdrew: calculate the number of experiments that each pair of proteins share
-        feature_shared_bait_table_geneid = feature_shared_bait_table_geneid.reset_index()
-        #ks_geneid = feature_shared_bait_table_geneid.groupby('IDs_tup')['bait_id_column_str'].nunique()
-        ks_geneid = feature_shared_bait_table_geneid.groupby('gene_id2_str')['bait_id_column_str'].nunique()
-        print("ks_geneid")
-        print(ks_geneid)
-
-        for gene_id2 in ks_geneid.index:
-            k = ks_geneid[str(gene_id2)]
-            m = ms_values[str(gene_id2)]
-            n = ms_values[str(geneid)]
-
-            #kdrew: do not calculate for the same gene
-            if str(geneid) == str(gene_id2):
-                continue
-
-            if transform_pvalue:
-                try:
-                    exponents = hypergeometric_exponents(k,n,m,N)
-                    output_dict2['gene_id1'].append(str(geneid))
-                    output_dict2['gene_id2'].append(str(gene_id2))
-                    output_dict2['pair_count'].append(k)
-                    output_dict2['neg_ln_pval'].append(np.nan)
-                    output_dict2['pval'].append(np.nan)
-                    output_dict2['exponents'].append(exponents)
-                except Exception as e:
-                    print("Exception (%s, %s): %s" % (geneid, gene_id2, str(e)))
-                    continue
-
-            else:
-                try:
-                    p = pval(k,n,m,N, denm=denm, logchoose=logchoose)
-                    try:
-                        neg_ln_p = -1.0*math.log(p)
-                    except ValueError as ve:
-                        print(str(ve))
-                        neg_ln_p = np.nan
-
-                    #print("calculated pval")
-                    #print("%s,%s k:%s m:%s n:%s N:%s -ln(p):%s" % (geneid, gene_id2, k, m, n, N, neg_ln_p))
-
-                    output_dict2['gene_id1'].append(str(geneid))
-                    output_dict2['gene_id2'].append(str(gene_id2))
-                    output_dict2['pair_count'].append(k)
-                    output_dict2['neg_ln_pval'].append(neg_ln_p)
-                    output_dict2['pval'].append(p)
-                    output_dict2['exponents'].append(np.nan)
-
-                except Exception as e:
-                    #logger.info(str(e))
-                    print("Exception (%s, %s): %s" % (geneid, gene_id2, str(e)))
-                    continue
+    for gene_id_result in gene_id_results:
+        output_dict2['gene_id1'] = output_dict2['gene_id1'] + gene_id_result['gene_id1']
+        output_dict2['gene_id2'] = output_dict2['gene_id2'] + gene_id_result['gene_id2']
+        output_dict2['pair_count'] = output_dict2['pair_count'] + gene_id_result['pair_count']
+        output_dict2['neg_ln_pval'] = output_dict2['neg_ln_pval'] + gene_id_result['neg_ln_pval']
+        output_dict2['pval'] = output_dict2['pval'] + gene_id_result['pval']
+        output_dict2['exponents'] = output_dict2['exponents'] + gene_id_result['exponents']
 
 
     #kdrew: find value to transform by (ie. median exponent of all exponents) and transform_exponents for every entry
     if transform_pvalue:
-        #kdrew: find median of exponents for each individual entry and then find the median of those
-        median_value = np.median([np.median(ex) for ex in output_dict2['exponents']])
-        print("median_value")
-        print(median_value)
+        #kdrew: find min of exponents for each individual entry and then find the min of those
+        min_value = np.min([np.min(ex) for ex in output_dict2['exponents']])
+        print("min_value: %s" % min_value)
+        #kdrew: normalize the min exponent to the SMALL_EXPONENT (-745.1332) which is the smallest exponent before scipy.exp(x) becomes 0.0
+        #kdrew: this has the effect of shifting all of the small pvalues (~0.0) that evaluate to 0.0 to be within the machine precision
+        transform_value = min_value - SMALL_EXPONENT 
+        print("transform_value: %s" % transform_value)
                              
-        output_dict2['pval'] = [transform_exponents(ex, median_value, calc_true_pval=False) for ex in output_dict2['exponents']]
+        #kdrew: calculate pvalue from exponents, transforming them if specified
+        output_dict2['pval'] = [transform_exponents(ex, transform_value, calc_true_pval=False) for ex in output_dict2['exponents']]
+        if remove_exponents:
+            #kdrew: remove exponents, no longer needed
+            output_dict2.pop('exponents')
 
         #output_dict2['neg_ln_pval'] = [-1.0*math.log(pvalue) for pvalue in output_dict2['pval']]
         output_dict2['neg_ln_pval'] = []
@@ -294,6 +257,87 @@ def shared_bait_feature(feature_table, bait_id_column, id_column, bh_correct=Fal
 
     output_df = pd.DataFrame(output_dict2)
     return output_df
+
+
+def shared_bait_feature_helper(geneid, feature_table, id_column, ms_values, N, denm, logchoose, transform_pvalue):
+    print("geneid: %s" % geneid)
+    #kdrew: slice feature_table to only have a single id
+    feature_table_geneid = feature_table[feature_table[id_column] == geneid] 
+    #kdrew: join the sliced feature table with the entire feature table on the experiment column (ie. bait_id_column_str)
+    feature_shared_bait_table_geneid = feature_table_geneid.join(feature_table, rsuffix="_right")
+    #print (feature_table_geneid)
+
+    #kdrew: make ids into strings and generate tuples of pairs of genes
+    feature_shared_bait_table_geneid['gene_id1_str'] = feature_shared_bait_table_geneid[id_column].apply(str)
+    feature_shared_bait_table_geneid['gene_id2_str'] = feature_shared_bait_table_geneid[id_column+'_right'].apply(str)
+    feature_shared_bait_table_geneid['IDs'] = map(sorted, zip(feature_shared_bait_table_geneid['gene_id1_str'].values, feature_shared_bait_table_geneid['gene_id2_str'].values))
+    feature_shared_bait_table_geneid['IDs_tup'] = feature_shared_bait_table_geneid['IDs'].apply(tuple)
+    print (feature_shared_bait_table_geneid)
+
+    #kdrew: calculate the number of experiments that each pair of proteins share
+    feature_shared_bait_table_geneid = feature_shared_bait_table_geneid.reset_index()
+    #ks_geneid = feature_shared_bait_table_geneid.groupby('IDs_tup')['bait_id_column_str'].nunique()
+    ks_geneid = feature_shared_bait_table_geneid.groupby('gene_id2_str')['bait_id_column_str'].nunique()
+    print("ks_geneid")
+    print(ks_geneid)
+
+    return_output_dict2 = dict()
+    return_output_dict2['gene_id1'] = []
+    return_output_dict2['gene_id2'] = []
+    return_output_dict2['pair_count'] = []
+    return_output_dict2['neg_ln_pval'] = []
+    return_output_dict2['pval'] = []
+    return_output_dict2['exponents'] = []
+
+    for gene_id2 in ks_geneid.index:
+        k = ks_geneid[str(gene_id2)]
+        m = ms_values[str(gene_id2)]
+        n = ms_values[str(geneid)]
+
+        #kdrew: do not calculate for the same gene
+        if str(geneid) == str(gene_id2):
+            continue
+
+        if transform_pvalue:
+            try:
+                exponents = hypergeometric_exponents(k,n,m,N)
+                return_output_dict2['gene_id1'].append(str(geneid))
+                return_output_dict2['gene_id2'].append(str(gene_id2))
+                return_output_dict2['pair_count'].append(k)
+                return_output_dict2['neg_ln_pval'].append(np.nan)
+                return_output_dict2['pval'].append(np.nan)
+                return_output_dict2['exponents'].append(exponents)
+            except Exception as e:
+                print("Exception (%s, %s): %s" % (geneid, gene_id2, str(e)))
+                continue
+
+        else:
+            try:
+                p = pval(k,n,m,N, denm=denm, logchoose=logchoose)
+                try:
+                    neg_ln_p = -1.0*math.log(p)
+                except ValueError as ve:
+                    print(str(ve))
+                    neg_ln_p = np.nan
+
+                #print("calculated pval")
+                #print("%s,%s k:%s m:%s n:%s N:%s -ln(p):%s" % (geneid, gene_id2, k, m, n, N, neg_ln_p))
+
+                return_output_dict2['gene_id1'].append(str(geneid))
+                return_output_dict2['gene_id2'].append(str(gene_id2))
+                return_output_dict2['pair_count'].append(k)
+                return_output_dict2['neg_ln_pval'].append(neg_ln_p)
+                return_output_dict2['pval'].append(p)
+                return_output_dict2['exponents'].append(np.nan)
+
+            except Exception as e:
+                #logger.info(str(e))
+                print("Exception (%s, %s): %s" % (geneid, gene_id2, str(e)))
+                continue
+
+    return return_output_dict2
+
+
 
 if __name__ == "__main__":
     main()
