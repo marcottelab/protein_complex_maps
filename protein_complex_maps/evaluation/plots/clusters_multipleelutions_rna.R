d16af120ef30bdf915dee2c9ec7f218fb2455af8
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2018-07-06 17:12:00 -0500

    Add new R sparkline script

---
+++ protein_complex_maps/evaluation/plots/clusters_multipleelutions_rna.R
@@ -0,0 +1,130 @@
+library(stringr)
+library(tidyr)
+library(argparse)
+library(dplyr)
+library(readr)  #for read_csv instead of read.csv
+library(ggplot2)
+library(cowplot) #for formattting
+library(ggridges) #for sparklines
+library(purrr) #for map function
+library(grid)
+library(gridExtra) #For annotation table under sparklines
+
+
+parser <- ArgumentParser(description='Make many sparkline figures, grouped by complex number')
+
+#parser$add_argument('--group_elut_norm', dest='group_elut_norm', action='store', required=TRUE, help='Annotated, normalized tidy elution file')
+#parser$add_argument('--expid', dest='expid', action='store', required=TRUE, help='Base name for output files')
+#parser$add_argument('--annotations', dest='annotations', action='store', required=TRUE, help='virNOG_collapse_annotations')
+
+parser$add_argument("-f", "--filenames", nargs='+', dest="filenames", required=TRUE,
+                        help="Input elution profile")
+parser$add_argument("-o", "--output_filename", action="store", required=TRUE,
+                        dest="output_filename", help="Output filename for resulting plot")
+parser$add_argument("-p", "--proteins", nargs='+', dest="protein_ids", required=TRUE,
+                        help="Proteins to plot")
+parser$add_argument("-n", "--parse_fraction_name", nargs='+', dest="parse_fraction_name", required=TRUE,
+                        help="Fields to separate fraction name into, example: cell_type condition col_type fraction date, use -n fraction for default behavior")
+parser$add_argument("-m", "--id_mapping", dest="id_mapping", required=TRUE,
+                        help="Uniprot mapping file")
+parser$add_argument("-g", "--id_column", dest="id_column", default="genename", help="Broken, has to be genename. Column to use for protein identifier, default = genename")
+parser$add_argument("-s", "--fraction_name_sep", dest="fraction_name_sep", default='_', help="Separator to separate fraction name, default = _")
+
+
+
+args = parser$parse_args()
+
+normalit<-function(m){
+    (m - min(m))/(max(m)-min(m))
+}
+
+
+complex_plot <- function(tidy_elution){
+     #Plot one complex at a time, by complex ID
+
+        
+     plt <- ggplot(data = tidy_elution, aes(x = as.numeric(as.character(fractionnum)), y = ID, height =  norm_speccounts, group = paste(ID, condition), color = condition )) +
+        geom_ridgeline(stat = "identity", alpha = 0, scale = 0.9, size = 0.3) +
+        theme_ridges() +
+        scale_color_manual(values = c("black", "red")) +
+        scale_linetype_manual(values=c("solid","longdash")) +
+          #  ggtitle(label = paste("Cluster", as.character(clusterID))) +
+        scale_y_discrete(c(0,1)) +
+        theme(axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.y= element_text(size = 8)) +
+        theme(legend.position = "top") + 
+        theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())  +
+        #theme(panel.spacing.x = unit(0.1, "lines")) + 
+        #facet_wrap(~spec + experiment_type + tissue,  nrow=1, scales="free_x")+
+        NULL 
+     return(plt)
+    
+}
+ 
+print(args)
+
+full_exp_tidy <- data.frame(fraction = character(), speccounts = numeric(), ID = character())
+for(fname in args$filenames){
+
+   exp <- read_delim(fname, delim = "\t")
+   exp <- exp %>% rename(ID = `#ProtID`) %>% select(-TotalCount)      
+   #ProtID TotalCount      
+   
+   exp_tidy <- exp %>% gather(fraction, speccounts, -ID)
+   if(args$parse_fraction_name[1] != "fraction") {
+      exp_tidy <- exp_tidy %>% separate(fraction, into = args$parse_fraction_name, sep = args$fraction_name_sep, remove = FALSE)
+
+    }
+   full_exp_tidy <- rbind(full_exp_tidy, exp_tidy)
+}
+
+print(full_exp_tidy)
+
+
+filt_exp_tidy <- full_exp_tidy %>% filter(ID %in% args$protein_ids)
+filt_exp_tidy <- filt_exp_tidy %>% group_by(ID) %>%
+                                   mutate(norm_speccounts = normalit(speccounts)) %>%
+                                   ungroup
+
+filt_exp_tidy <- filt_exp_tidy %>% mutate(fractionnum = str_extract(fractionnum,"(\\d)+"))
+
+
+#Add on uniprot annotation file
+mapping_df <- read_delim(args$id_mapping,  ",")
+print(mapping_df)
+filt_exp_tidy <- filt_exp_tidy %>% left_join(mapping_df, by = c("ID" = "Entry"))
+print("joined")
+
+#Make new column name for selected column
+print(args$id_column)
+print(filt_exp_tidy[,args$id_column])
+
+
+#Fixed as genename
+filt_exp_tidy$ID2 <- filt_exp_tidy$genename #filt_exp_tidy[,args$id_column] 
+print(filt_exp_tidy)
+print('here')
+
+
+#print(filt_exp_tidy)
+#Replace any missing IDs 
+#print(filt_exp_tidy)
+#filt_exp_tidy$ID2[is.na(filt_exp_tidy$ID2)] <- filt_exp_tidy$ID
+#print(filt_exp_tidy)
+
+#Overwrite the original ID column with selected ID column
+print("bla")
+print(filt_exp_tidy)
+print(filt_exp_tidy$ID)
+print(filt_exp_tidy$ID2)
+
+
+filt_exp_tidy$ID <- filt_exp_tidy$ID2
+
+print(filt_exp_tidy)
+
+print("starting plotting")
+clusterplot <- complex_plot(filt_exp_tidy)
+
+ggsave(args$output_filename, plot = clusterplot)
+
+
