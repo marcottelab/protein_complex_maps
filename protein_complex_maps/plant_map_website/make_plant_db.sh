a7758032ae6171c9f1d87cdbc206715457a9f745
clairemcwhite <claire.mcwhite@utexas.edu>
2019-06-11 20:36:31 -0500

    semi working, but needs two step query

--- protein_complex_maps/plant_map_website/make_plant_db.sh
+++ protein_complex_maps/plant_map_website/make_plant_db.sh
@@ -1,1 +1,2 @@
-python load_clusters.py --cluster_table data/allplants_feature_matrix_missing1.unscaled.top100.edges.3steps.fdr10.threshold504.walktrap.csv             
+python load_clusters.py --cluster_table static/data/allplants_feature_matrix_missing1.unscaled.top100.edges.3steps.fdr10.threshold504.walktrap.csv
+python load_conversion.py --conversion_file static/data/plant_virNOG_orthology.csv       
