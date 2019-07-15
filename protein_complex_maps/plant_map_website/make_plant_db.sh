f51e695872efb5225bc886fbf69be3614c403f5a
clairemcwhite <claire.mcwhite@utexas.edu>
2019-07-15 15:13:21 -0500

    Try adding more large files for making DB

--- protein_complex_maps/plant_map_website/make_plant_db.sh
+++ protein_complex_maps/plant_map_website/make_plant_db.sh
@@ -1,12 +1,13 @@
 echo "Loading OrthogroupIDs"
 python scripts/load_ortho.py --orthogroup_file static/data/all_OrthogroupIDs.txt
-#Then load rest. Since Orthogroup and Protein IDs already exist and don't have to be created, the rest goes faster
+#Then load rest. Since Orthogroup IDs already exist and don't have to be created, the rest goes faster
 echo "Loading clusters"
 #Each orthogroup belongs in multiple clusters.
 python scripts/load_clusters.py --cluster_table static/data/allplants_feature_matrix_missing1.unscaled.top100.edges.3steps.fdr10.threshold504.walktrap.csv
 echo "Load orthogroup annotation"
 python scripts/load_orthoannot.py --annotation_file static/data/virNOG_collapse_annotations.txt 
 echo "Load CF-MS scores"
+gunzip static/data/allplants_feature_matrix_missing1.unscaled.top100.edges.top10k.gz
 python scripts/load_scores.py --score_file static/data/allplants_feature_matrix_missing1.unscaled.top100.edges.top10k 
 echo "Load Orthogroup Protein conversion"
 python scripts/load_prot.py --conversion_file static/data/plant_virNOG_orthology.csv    
