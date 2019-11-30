d573d9f3d52f15514e1e9b0efb5fd9c4e21507de
Kevin Drew <kdrew@utexas.edu>
2019-11-29 23:20:35 -0600

    load_enrichment_db: updated readin of enrichment to use pandas

--- protein_complex_maps/complex_map_website/load_enrichment_db.py
+++ protein_complex_maps/complex_map_website/load_enrichment_db.py
@@ -2,6 +2,7 @@
 import argparse
 import numpy as np
 import itertools as it
+import pandas as pd
 
 import csv
 
@@ -20,53 +21,35 @@ def main():
 
     db.create_all()
 
-    enrichment_table_file = open(args.enrichment_file,"rb")
-    complex_id = None
-    for line in enrichment_table_file.readlines():
+    enrichment_df = pd.read_csv(args.enrichment_file)
+    for _, row in enrichment_df.iterrows():
 
-        if 'complex:' in line:
-            complex_id = line.split()[1]
-            continue
+        #index,complex_id,description,effective_domain_size,goshv,group_id,intersection_genes,intersection_size,name,native,p_value,parents,precision,query,query_size,recall,significant,source,source_order,term_size
 
-        #kdrew: if header do not parse
-        #  signf   corr. p-value   T   Q   Q&T Q&T/Q   Q&T/T   term ID     t type  t group    t name and depth in group        Q&T list
-        if 'signf' in line:
-            continue
-
-        print line
-        split_line = line.split('\t')
-        print split_line
-
-        corr_pval = split_line[2]
-        t_count = split_line[3] 
-        q_count = split_line[4] 
-        qandt_count = split_line[5]  
-        qandt_by_q = split_line[6] 
-        qandt_by_t = split_line[7] 
-        term_id = split_line[8] 
-        t_type = split_line[9] 
-        t_group = split_line[10] 
-        t_name = split_line[11] 
-        depth_in_group = split_line[12] 
-        qandt_list = split_line[13].strip()
-
-        c = db.session.query(cdb.Complex).filter_by(complex_id=complex_id).first()
+        c = db.session.query(cdb.Complex).filter_by(complex_id=row.complex_id).first()
         if c:
             print "complex id: %s" % c.id
             ce = cdb.get_or_create(db, cdb.ComplexEnrichment, 
-                                        complex_key=c.id, 
-                                        corr_pval=corr_pval, 
-                                        t_count=t_count, 
-                                        q_count=q_count, 
-                                        qandt_count=qandt_count, 
-                                        qandt_by_q=qandt_by_q, 
-                                        qandt_by_t=qandt_by_t, 
-                                        term_id=term_id, 
-                                        t_type=t_type, 
-                                        t_group=t_group, 
-                                        t_name=t_name, 
-                                        depth_in_group=depth_in_group, 
-                                        qandt_list=qandt_list)
+                complex_key           = c.id,
+                description           = row.description,
+                effective_domain_size = row.effective_domain_size,
+                goshv                 = row.goshv,                
+                group_id              = row.group_id,             
+                intersection_genes    = row.intersection_genes,    
+                intersection_size     = row.intersection_size,    
+                name                  = row.name,                 
+                native                = row.native,               
+                p_value               = row.p_value,              
+                parents               = row.parents,              
+                precision             = row.precision,            
+                query                 = row.query,                
+                query_size            = row.query_size,           
+                recall                = row.recall,               
+                significant           = row.significant,          
+                source                = row.source,               
+                source_order          = row.source_order,         
+                term_size             = row.term_size)             
+
             db.session.add(ce)
             db.session.commit()
         else:
