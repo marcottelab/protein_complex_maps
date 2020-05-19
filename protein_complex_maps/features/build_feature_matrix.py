496e7e864b6ee0e6c611086ff6dae2847e30c216
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2020-05-19 17:10:57 -0500

    Fix problems with build_feature_matrix

--- protein_complex_maps/features/build_feature_matrix.py
+++ protein_complex_maps/features/build_feature_matrix.py
@@ -21,10 +21,12 @@ def build_matrix():
                                     help="If provided, stores an intermediate features matrix every x files joined")
     parser.add_argument("--output_file", action="store", dest="out_filename", required=True, default=None, 
                                     help="Filename of output file")
+    parser.add_argument("--header", action="store", dest="header", required=False, default=True, 
+                                    help="Do input pairs files have headers?")
+
     args = parser.parse_args()
     
     dfall = args.dfall
-
 
 
     print(dfall)
@@ -32,16 +34,10 @@ def build_matrix():
         dfall = pd.DataFrame(columns=['ID'])
      
     else:
-   	 dfall = pd.read_table(dfall, sep=",")
-         print(dfall)
+        dfall = pd.read_table(dfall, sep=",")
+        print(dfall)
         
-         #dfall['pairset'] = map(frozenset, zip( dfall['ID1'].values, dfall['ID2'].values ))
-         #a['IDHash'] = a.ID.apply(lambda r: tuple(sorted(r.iteritems())))
-         #b['IDHash'] = b.ID.apply(lambda r: tuple(sorted(r.iteritems())))
-         #print("dfall index set")
-    #Claire: haven't checked this, but allow features to be added onto existing feature matrix. 
     #kdrew: read in pairs files
-    #dfall = pd.DataFrame(columns=['pairset'])
     dfall = dfall.set_index(['ID'])
     out_columns = []
 
@@ -50,7 +46,13 @@ def build_matrix():
         print(i, filename)
         value_name = '.'.join(os.path.basename(filename).split('.')[:-2])
         out_columns.append(value_name)
-        df = pd.read_csv(filename, delimiter=args.sep, header=None, names=['A', 'B', value_name])
+     
+        if args.header == True:
+            df = pd.read_csv(filename, delimiter=args.sep, header=0, names=['A', 'B', value_name])
+        elif args.header == False:
+            df = pd.read_csv(filename, delimiter=args.sep, header=None, names=['A', 'B', value_name])
+
+
         print(df)
         df['ID']=df.apply(lambda x:'%s %s' % (x['A'],x['B']),axis=1)
         print("new column made")
@@ -59,45 +61,28 @@ def build_matrix():
         df = df_intermediate
         print(df)
 
-        #df.columns = ['ID',value_name])
-
-        #print(filename)
         if value_name in dfall.columns:
             dfall  = dfall.drop(value_name, 1)
         
-        #kdrew: create frozenset column with id1 and id2
-        #df['pairset'] = map(frozenset, zip( df['id1'].values, df['id2'].values ))
-        #print(df)
-        #claire get rid of ID columns
-        #df = df[['pairset', value_name]]
         df = df.set_index(['ID'])
         print(df.shape)
         dfall_intermediate = dfall.join(df, how='outer', rsuffix=('_%s' % (i) ))
-        #dfall_intermediate = dfall.join(df, how='outer', rsuffix=('_%s' % (i) ))
         dfall = None
         dfall = dfall_intermediate
         dfall_intermediate = None
         df = None
- 
-        if i % args.interval == 0:
-             int_out_filename = "interval_" + str(i) + "_" + args.out_filename
-             dfall.to_csv(int_out_filename)
+  
+        # Save an intermediate file for long running joins
+        if args.interval: 
+            if i % args.interval == 0:
+                if i > 0:
+                    int_out_filename = args.out_filename +  ".interval_" + str(i) 
+                    dfall.to_csv(int_out_filename)
 
            
-        #print(dfall)
-        #print(df)
         print(dfall.shape)
-        #kdrew: outer merge dfs on frozenset column
-        #dfall = dfall.merge(df, how='outer', on='pairset', suffixes=('', '_%s' % (i) ))
 
     dfall = dfall.reset_index()
-    #dfall['ID1'] = [list(x)[0] for x in dfall['pairset'].values]
-    #dfall['ID2'] = [list(x)[1] for x in dfall['pairset'].values]
-
-    #kdrew: prepend global id columns to out vector
-    #out_columns = ['ID1','ID2'] + out_columns
-
-    #dfall = dfall.fillna(0.0)
 
     #kdrew: write to file 
     dfall.to_csv(args.out_filename, index=False)
