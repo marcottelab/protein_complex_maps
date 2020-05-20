57022967b418a5f20da4c98ebab7e23bf234579f
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2020-05-19 21:55:09 -0500

    Fix to allow list of features either in a file or with nargs

--- protein_complex_maps/features/build_feature_matrix.py
+++ protein_complex_maps/features/build_feature_matrix.py
@@ -10,16 +10,20 @@ import os.path
 
 def build_matrix():
 
-    parser = argparse.ArgumentParser(description="Builds feature matrix by concatenating pairs files")
-    parser.add_argument("--input_pairs_files", action="store", dest="pairs_files", nargs='+', required=True, 
-                                    help="Filenames of pairs files, format: id1 id2 value")
+    parser = argparse.ArgumentParser(description="Builds feature matrix by concatenating pairs files (format: ID1 ID2 value")
+    parser.add_argument("--input_pairs_files", action="store", dest="pairs_files", nargs='+', required=False, 
+                                    help="Filenames of pairs files, space separated string")
+
+    parser.add_argument("--input_pairs_list", action="store", dest="pairs_file_list", required=False, 
+                                    help="File of pairs filenames, one per line")
+
     parser.add_argument("--sep", action="store", dest="sep", required=True, 
                                     help="Separator for input files")
 
     parser.add_argument("--prev_feature_matrix", action="store", nargs='?', dest='dfall', required=False)
     parser.add_argument("--store_interval", action="store", dest="interval", type=int, required=False, default=None, 
                                     help="If provided, stores an intermediate features matrix every x files joined")
-    parser.add_argument("--output_file", action="store", dest="out_filename", required=True, default=None, 
+    parser.add_argument("--output_file", action="store", dest="out_filename", required=True, 
                                     help="Filename of output file")
     parser.add_argument("--header", action="store", dest="header", required=False, default=True, 
                                     help="Do input pairs files have headers?")
@@ -28,21 +32,35 @@ def build_matrix():
     
     dfall = args.dfall
 
+    if not args.pairs_files:
+         if not args.pairs_file_list:
+            print("either --input_pairs or --input_pairs_file required")
+            return()
 
     print(dfall)
     if not dfall:
         dfall = pd.DataFrame(columns=['ID'])
      
     else:
-        dfall = pd.read_table(dfall, sep=",")
+        dfall = pd.read_csv(dfall)
         print(dfall)
         
+    print("interval", args.interval) 
+
     #kdrew: read in pairs files
     dfall = dfall.set_index(['ID'])
     out_columns = []
 
 
-    for i, filename in enumerate(args.pairs_files):
+    if args.pairs_files:
+           pairs_files = args.pairs_files
+    if args.pairs_file_list:
+           pairs_files = pd.read_csv(args.pairs_file_list, header = None, names = ["filenames"])["filenames"].tolist()
+
+
+
+    print(pairs_files)
+    for i, filename in enumerate(pairs_files):
         print(i, filename)
         value_name = '.'.join(os.path.basename(filename).split('.')[:-2])
         out_columns.append(value_name)
@@ -76,7 +94,7 @@ def build_matrix():
         if args.interval: 
             if i % args.interval == 0:
                 if i > 0:
-                    int_out_filename = args.out_filename +  ".interval_" + str(i) 
+                    int_out_filename = args.out_filename +  ".intermediate_" + str(i) 
                     dfall.to_csv(int_out_filename)
 
            
